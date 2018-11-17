e76cafd4d77318c756534009e89d789d671865e4
coryrgriffith <corygriffith9@gmail.com>
2018-11-17 15:02:11 -0600

    add total_coin_value method to calculate total value of all coins in the system

--- app/models/coin.rb
+++ app/models/coin.rb
@@ -1,2 +1,10 @@
 class Coin < ApplicationRecord
+  def total_coin_value
+    all_coins = Coin.all
+    total_value = 0
+    all_coins.each do |coin|
+      total_value += coin.value
+    end
+    return total_value.to_f
+  end
 end
