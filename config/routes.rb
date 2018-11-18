267067bce18481f1a17a588d94a9038b5d4f037f
coryrgriffith <corygriffith9@gmail.com>
2018-11-18 11:57:50 -0600

    add account controller to handle creation and and deletion of Api Key and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   resources :coins
+
+  match '/signin', to: 'account#signin', via: 'get'
+  match '/signout', to: 'account#signout', via: 'get'
   # STEP 1: A ROUTE triggers a controller action
   # verb "/urls" => "namespace/controllers#action"
 end
